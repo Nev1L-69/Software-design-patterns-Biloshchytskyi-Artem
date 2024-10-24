06409ff94a6911c24442c9c7422b1482a0189d0b
NeviL <baa20.10.2005@gmail.com>
2024-10-24 15:48:34 +0500

    Assignment 4,add Read me

---
+++ Assignment 4/Assignment 4.1/src/ShoppingCart.java
@@ -0,0 +1,15 @@
+public class ShoppingCart {
+    private PaymentStrategy paymentStrategy;
+
+    public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void checkout(double amount) {
+        if (paymentStrategy == null) {
+            System.out.println("Please select a payment method.");
+        } else {
+            paymentStrategy.processPayment(amount);
+        }
+    }
+}
